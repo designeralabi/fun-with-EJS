acc7ad53e5fbfb4bca348f3f630d63670da66b34
designeralabi <bdlkrmalabi@gmail.com>
2021-12-29 01:58:30 -0800

    other page routing fix

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ const port = 3000;
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    // res.send('Hello World!');
     res.render('index');
 });
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
