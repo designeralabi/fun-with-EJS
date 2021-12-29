c4829e1dffb3f0b20394b098564918763e32b6a2
designeralabi <bdlkrmalabi@gmail.com>
2021-12-29 01:51:59 -0800

    first-commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const ejs  = require ('ejs');
+const port = 3000;
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    // res.send('Hello World!');
+    res.render('index');
+
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
