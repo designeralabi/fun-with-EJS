ebd5cf5400e8a4af978c56d4196b049728ff62eb
designeralabi <bdlkrmalabi@gmail.com>
2021-12-29 01:56:10 -0800

    content added to other ejs pages

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     // res.send('Hello World!');
     res.render('index');
-
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
